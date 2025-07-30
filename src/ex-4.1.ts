a1732af4ed9b140d95e353932477785163782d2f
PTAE43 <kritsada1696@gmail.com>
2025-07-30 17:18:44 +0700

    feat: Ex.4.1

--- src/ex-4.1.ts
+++ src/ex-4.1.ts
@@ -1,14 +1,15 @@
 type User = {
   id: string;
   name: string;
-  age?: number;
+  age: number; //ลบ ?
 };
 
 function isAdult(user: User): boolean {
   return user.age >= 18;
 }
 
-const result = isAdult({ id: "u01", name: "John" });
+const result = isAdult({ id: "u01", name: "John" , age: 17}); //เพิ่มอายุ
 console.log(result); // ควรได้ false
 
-// Error ที่เจอคือ
+// Error ที่เจอคือ parametor user มีค่าเป็น undefined 
+// ทำให้การกำหนด ? ไว้ใน age จึงทำให้เกิด error
